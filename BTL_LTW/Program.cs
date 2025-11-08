e9c62886072f26427bed3c8e5958a650140d0acf
NgLongVu <vutthhbg05@gmail.com>
2025-11-08 15:40:38 +0700

    Hieu

--- BTL_LTW/Program.cs
+++ BTL_LTW/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddDbContext<MaleFashionContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("MaleFashion"));
 });
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +32,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
